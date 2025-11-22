75d154d2fb69aadad2fcb933ffc660843463924c
FluffyPlume <realkapayapaan@gmail.com>
2025-11-22 20:31:07 +0800

    Update DuplicateEntryException.java

--- src/main/java/org/example/flowerapp/Exceptions/DuplicateEntryException.java
+++ src/main/java/org/example/flowerapp/Exceptions/DuplicateEntryException.java
@@ -7,7 +7,6 @@ public class DuplicateEntryException extends RuntimeException {
         super(String.format("The flower named '%s', species '%s', and planted on %tB %te, %tY already exists in the system.", name, species, date, date, date));
     }
 
-
     public DuplicateEntryException(String message) {
         super(message);
     }
